4ec513c9c2b056cdd13587b9f5f88663a37b4d9f
Sachin Rastogi(sachin.rastogi) <sachin.rastogi@infosys.com>
2024-03-23 22:55:56 +0530

    Initial Commit Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        customFont: ["Roboto", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
